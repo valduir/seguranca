b6ca397942a05c97276c8dd5c906a0874c43bbc8
valduir <128056466+valduir@users.noreply.github.com>
2024-04-10 11:04:49 -0300

    pasta

---
+++ main.js
@@ -0,0 +1,68 @@
+document.querySelector('.parametro-senha__texto');
+    const numeroSenha = document.querySelector('.parametro-senha__texto');
+    const numeroSenha = document.querySelector('.parametro-senha__texto');
+    numeroSenha.textContent = 5;
+    const botoes = document.querySelector('.');
+    const botoes = document.querySelector('.parametro-senha__botao');
+    const botoes = document.querySelector('.parametro-senha__botao');
+
+    console.log(botoes);
+    const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+
+function diminuiTamanho(){ 
+
+}
+let tamanhoSenha = 12;
+senha.textContent = tamanhoSenha;
+tamanhoSenha = tamanhoSenha - 1;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+}function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+
+
+console.log(botoes);
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+    …    .barra{
+            background-color: var(--fundo-senha);
+            height: 30px;
+            width: 100%;
+        }
+        .forca{
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            // tamanhoSenha = tamanhoSenha + 1;
+            tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
